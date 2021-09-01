3fd4134631bf17f6d96e31422f94ad980cc2b185
Andrea Allen <andrea2allen@gmail.com>
2021-09-01 16:12:54 -0400

    Release version 1.0.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages
 import setuptools
 
-VERSION = '0.1.1'
+VERSION = '1.0.1'
 DESCRIPTION = 'EpIntervene Simulation Package'
 LONG_DESCRIPTION = 'EpIntervene package for simulating SIR and SEIR Epidemics with or without interventions'
 
